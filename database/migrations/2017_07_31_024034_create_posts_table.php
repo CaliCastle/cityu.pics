5a713c4a756e80586acd03b922dfcd9a37cf3eca
Cali Castle <cali@calicastle.com>
2017-07-30 21:11:25 -0700

    Complete database migration design

--- database/migrations/2017_07_31_024034_create_posts_table.php
+++ database/migrations/2017_07_31_024034_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('experience');
-            $table->rememberToken();
+            $table->unsignedInteger('user_id');
+            $table->text('content');
             $table->timestamps();
         });
+
+//        Schema::create('post_likes', function (Blueprint $table) {
+//
+//        });
     }
 
     /**
@@ -31,6 +32,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+//        Schema::dropIfExists('post_likes');
+        Schema::dropIfExists('posts');
     }
 }
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,15 @@
+@include('layouts.header')
+<body class="auth">
+
+    <div class="container">
+
+        <div class="hero col-md-7">
+
+        </div>
+        <div class="main col-md-5">
+            @yield('content')
+        </div>
+    </div>
+
+</body>
+</html>
